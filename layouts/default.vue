c0f434cee3b7593f3ea2d8a8023585fec5b6b3e7
lnoueryo <popo62520908@gmail.com>
2021-05-20 02:45:06 +0900

    チャートの分割数を動的にする機能及びレスポンシブに対応

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app dark>
+    <v-app dark v-resize="onResize">
         <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
             <v-list>
                 <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
@@ -50,33 +50,43 @@
     </v-app>
 </template>
 
-<script>
-export default {
-data() {
-    return {
-    clipped: false,
-    drawer: false,
-    fixed: false,
-    items: [
-        {
-        icon: 'mdi-apps',
-        title: 'Welcome',
-        to: '/',
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            items: [
+                {
+                    icon: 'mdi-apps',
+                    title: 'Welcome',
+                    to: '/',
+                },
+                {
+                    icon: 'mdi-chart-bubble',
+                    title: 'Login',
+                    to: '/login',
+                },
+            ],
+            miniVariant: false,
+            right: true,
+            rightDrawer: false,
+            title: 'Vuetify.js',
+            windowSize: {x: 0, y: 0 }
+        }
+    },
+    beforeCreate(){
+        this.$store.dispatch('isAuth', this.$route.name)
+    },
+    mounted () {
+        this.onResize()
+    },
+    methods:{
+        onResize () {
+            this.$store.dispatch('windowSize', { x: window.innerWidth, y: window.innerHeight })
         },
-        {
-        icon: 'mdi-chart-bubble',
-        title: 'Login',
-        to: '/login',
-        },
-    ],
-    miniVariant: false,
-    right: true,
-    rightDrawer: false,
-    title: 'Vuetify.js',
     }
-},
-beforeCreate(){
-    this.$store.dispatch('isAuth', this.$route.name)
-},
-}
+})
 </script>
