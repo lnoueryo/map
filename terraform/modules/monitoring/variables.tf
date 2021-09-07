cf749224475642e77642d508fff5d7d117828386
lnoueryo <popo62520908@gmail.com>
2021-09-08 04:19:24 +0900

    terraformファイル整理

---
+++ terraform/modules/monitoring/variables.tf
@@ -0,0 +1,9 @@
+variable "notification_email_address" {
+  type = string
+}
+variable "project_id" {
+  type = string
+}
+variable "host_name" {
+  type = string
+}
---
+++ terraform/modules/secret_manager/variables.tf
@@ -0,0 +1,6 @@
+variable "secret_id" {
+  type = string
+}
+variable "secret_data" {
+  type = string
+}
