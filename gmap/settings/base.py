382dab86089b1c64df550106dfdd3124ad86d1f1
lnoueryo <popo62520908@gmail.com>
2021-09-10 04:11:43 +0900

    デプロイ用

--- gmap/settings/base.py
+++ gmap/settings/base.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'gmap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'client/dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'client/dist/static'),
+    os.path.join(BASE_DIR, 'dist/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
