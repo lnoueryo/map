51a775571c5681f6cc427d13242d74d93c123379
lnoueryo <popo62520908@gmail.com>
2021-11-15 15:02:58 +0900

    tm-console調整

--- tm-console/nuxt.config.js
+++ tm-console/nuxt.config.js
@@ -43,7 +43,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/axios-accessor',
-    '~/plugins/map',
     '~/plugins/component'
   ],
 
