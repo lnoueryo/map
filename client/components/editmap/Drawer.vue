2706e95b817cceebcb4b24f84ad189b0046e576e
lnoueryo <popo62520908@gmail.com>
2021-11-16 02:23:57 +0900

    不要ファイル削除

--- client/components/editmap/Drawer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <div style="width:340px">
-            <toggle-switch key="1" class="mr-4" v-model="markerSwitch1" id="markerSwitch" background-color="#ff9800">マーカー</toggle-switch>
-            <text-box ref="textBox" :map="map" :switch="markerSwitch1"></text-box>
-        </div>
-    </div>
-</template>
